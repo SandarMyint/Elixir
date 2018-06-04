b3d236d902abab6e5e861d01184085b54a13d9f7
Asus <sandarmyint5897@gmail.com>
2018-06-04 18:23:02 +0630

    Elixir Tutorial

---
+++ lib/cards.ex
@@ -0,0 +1,27 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello
+      :world
+
+  """
+  def hello do
+    "world ! "
+  end
+
+def create_deck do
+  ["one","two","three"]
+end
+
+def shuffle(deck) do
+  
+end
+
+end
